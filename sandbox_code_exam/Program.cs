ca82787e0c298aa37bcfda2cc17c285ba3638159
Dennis <djl6347@g.rit.edu>
2022-04-01 14:38:51 -0400

    -completed ticket math based on user input -more logic on user input -need to do results screen

--- sandbox_code_exam/Program.cs
+++ sandbox_code_exam/Program.cs
@@ -22,14 +22,18 @@ namespace sandbox_code_exam
                 Console.WriteLine(entry.Key + ": " + entry.Value);
             }
 
+            Console.WriteLine("\n(To move back a step in the application, input 'back')\n(To end the day, input 'exit')");
+
             bool exit = false;
 
             while (!exit) {
+                int tickets;
                 bool verify = true;
                 Console.Write("\nWhich movie would you like to see today? ");
                 string userInput = Console.ReadLine().Trim().ToLower();
                 foreach (KeyValuePair<string, int> entry in screenings) {
                     if (userInput == entry.Key.ToLower()) { 
+                        userInput = entry.Key;
                         verify = true;
                         break;
                     }
@@ -40,10 +44,18 @@ namespace sandbox_code_exam
                     Console.WriteLine("That is not a movie we are currently showing.");
                     continue;
                 }
+                if (screenings[userInput] == 0) {
+                    Console.WriteLine("That movie is sold out on tickets.");
+                    continue;
+                }
                 while (true) {
                     Console.Write("How many tickets do you want? ");
                     try {
-                        int tickets = Convert.ToInt32(Console.ReadLine());
+                        tickets = Convert.ToInt32(Console.ReadLine());
+                        if (tickets > screenings[userInput]) {
+                            Console.WriteLine("That amount exceeds the available number of tickets!");
+                            continue;
+                        }
                         break;
                     }
                     catch (Exception e) {
@@ -51,11 +63,9 @@ namespace sandbox_code_exam
                         continue;
                     }
                 }
-
-                foreach(KeyValuePair<string, int> entry in screenings) {
-                    
-                }
-                
+                screenings[userInput] -= tickets;
+                Console.WriteLine("There are now " + screenings[userInput] + " tickets left for " + userInput);
+                sales += tickets;
             }
 
 
